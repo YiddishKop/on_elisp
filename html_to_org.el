a9a5dc40a3b9d8fe00aa67b50cf1576386d00951
yiddishkop@163.com <yiddishkop@163.com>
2018-07-30 18:51:52 +0800

    add some pcase introduction

--- html_to_org.el
+++ html_to_org.el
@@ -39,41 +39,41 @@
 
 
 
-
+;; part 1
 ;; 执行命令 from html to md, then to org
-
 ;; # from html to md
 ;; pandoc test.html -o test.md
-
 ;; # from jn to other format, eg markdown
 ;; jupyter nbconvert --to FORMAT notebook.ipynb
-
 ;; # from md to org
 ;; pandoc -f markdown -t org -o newfile.org original-file.markdown
-
 ;; Known bugs:
 ;; 对于 html 文档标题中带有 shell command 符号的文件转换会出现bug
 ;; 对于 超过 3M 的html 文档的转换会使得 pandoc 出现问题, 参考这里: https://github.com/jgm/pandoc/issues/352
 
+(defun single-html-to-org (htmlfile)
+  (setq pathwithoutext (file-name-sans-extension htmlfile))
+  (setq mdfile (concat pathwithoutext ".md"))
+  (setq orgfile (concat pathwithoutext ".org"))
+  (setq htmltomd (format "pandoc +RTS -K10240000 -RTS %s -o %s" htmlfile mdfile))
+  (message htmltomd)
+  (message (shell-command-to-string htmltomd))
+  (setq mdtoorg (format "pandoc +RTS -K10240000 -RTS -f markdown -t org -o %s %s" orgfile mdfile))
+  (message mdtoorg)
+  (message (shell-command-to-string mdtoorg))
+  )
+
+(setq test-html "/home/yiddi/Downloads/tensorflow_tutorial/xxx/Training_TensorFlow.html")
+(single-html-to-org test-html)
 
 (defun htmltoorg (lecdir)
   "convert all .html files under lecdir directory to org files"
   (dolist (htmlfile (directory-files lecdir t "html"))
-    (progn
-      (setq pathwithoutext (file-name-sans-extension htmlfile))
-      (setq mdfile (concat pathwithoutext ".md"))
-      (setq orgfile (concat pathwithoutext ".org"))
-      (setq htmltomd (format "pandoc +RTS -K10240000 -RTS %s -o %s" htmlfile mdfile))
-      (message htmltomd)
-      (message (shell-command-to-string htmltomd))
-      (setq mdtoorg (format "pandoc +RTS -K10240000 -RTS -f markdown -t org -o %s %s" orgfile mdfile))
-      (message mdtoorg)
-      (message (shell-command-to-string mdtoorg))
-      )))
+    (single-html-to-org htmlfile)))
 
 
 
-;; part two org 内容的改进
+;; part 2
 ;; 过多 <div> tag in org files
 ;; 检索 #+BEGIN_HTML 检索 #+END_HTML 将两者之间包含两者所在行都删除
 ;; example block modify to python src block
@@ -84,6 +84,47 @@
 
 ;; combine the two progn above together
 ;; and add a while loop to delete empty lines
+(defun del-single-html-and-modify-exmp (htmlfile)
+      (setq pathwithoutext (file-name-sans-extension htmlfile))
+      (setq orgfile (concat pathwithoutext ".org"))
+
+      ;; del html block
+      (set-buffer (find-file-noselect orgfile))
+      (goto-char 0)
+      (while (search-forward "#+BEGIN_HTML" nil t)
+        (progn
+          (beginning-of-line)
+          (setq beginpos (point))
+          (search-forward "#+END_HTML" nil t)
+          (end-of-line)
+          (setq endpos (point))
+          (delete-region beginpos endpos)
+          )
+        )
+      (message "delete")
+
+      ;; modify example block
+      (goto-char 0)
+      (while (search-forward "#+END_EXAMPLE" nil t)
+        (replace-match "#+END_SRC" nil t))
+      (goto-char 0)
+      (while (search-forward "#+BEGIN_EXAMPLE" nil t)
+        (replace-match "#+BEGIN_SRC ipython :tangle yes :session :exports code :async t :results raw drawer" t t))
+
+      ;; clean multiple empty lines with only 1 empty line
+      (goto-char 0)
+      (while (re-search-forward "\n\n\n+" nil "move")
+        (replace-match "\n\n"))
+
+
+      ;; save buffer
+      (save-buffer)
+      (kill-buffer)
+  )
+
+;; for test
+(del-single-html-and-modify-exmp test-html)
+
 (defun del-html-modify-exmp-block (lecdir)
   "convert all .html files under lecdir directory to org files"
   (dolist (htmlfile (directory-files lecdir t "html"))
@@ -128,6 +169,17 @@
 
 ;; part 3: delte the html and md files
 ;; elisp 的 regex 非常有意思,需要对符号进行两次转义
+(defun simple-del-html-md (dir)
+  (dolist (md-or-html (directory-files dir t ".html\\|.md\\|.ipython"))
+    (progn
+      (delete-file md-or-html)
+      )
+    )
+  )
+
+;; for testing
+(simple-del-html-md "/home/yiddi/Downloads/tensorflow_tutorial/xxx/")
+
 (defun del-html-and-md (path)
   (setq subdirs (directory-files path t))
 
@@ -229,6 +281,8 @@
   (del-dataimage-line path)
   (batch-tangle-org path)
   )
+(setq ttdir "/home/yiddi/Downloads/tensorflow tutorial/")
+(yid-html-to-org ttdir)
 
 (setq labdir "/home/yiddi/git_repos/on_ml_wushanghong/ml/labs/")
 (yid-html-to-org labdir)
